bfdb9c7395429342e11242713880630146a9bec7
slee-vista <81813363+slee-vista@users.noreply.github.com>
2022-07-20 12:54:42 -0400

    added html support

--- knight.js
+++ knight.js
@@ -1,55 +1,72 @@
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    return bgColor;
+}
+
+
+
 //console log the dvd logo bouncing around the screen
-//animate knight and move draw to console
-const gameboard = (height,width,knightpositiony,knightpositionx) => {
+//animate DVDlogo and move draw to console
+
+
+
+const gameboard = (height,width,DVDlogopositiony,DVDlogopositionx) => {
     let board = [];
     for (let i = 0; i < height; i++) {
         board[i] = [];
         for (let j = 0; j < width; j++) {
-            board[i][j] = '___';
+            board[i][j] = '`___`';
         }
     }
-    board[knightpositiony][knightpositionx] = 'DVD';
+    board[DVDlogopositiony][DVDlogopositionx] = 'DVD';
     return board;
 }
 
-//start knight at 0,0 and move to lower right corner with one move every second
+//start DVDlogo animation
 const animate = (gameboardheight,gameboardwidth) => {
-    let knightpositiony = 2;
-    let knightpositionx = 6;
-    let knightdirectiony = 1;
-    let knightdirectionx = 1;
+    let DVDlogopositiony = 2;
+    let DVDlogopositionx = 6;
+    let DVDlogodirectiony = 1;
+    let DVDlogodirectionx = 1;
     setInterval(() => {
-        //console clear
-        console.clear();
-        console.log(gameboard(gameboardheight,gameboardwidth,knightpositiony,knightpositionx).join('\n'));
+        //screen clear
+        document.querySelector('#gameboard').innerHTML = '';
+        document.querySelector('#gameboard').innerHTML = gameboard(gameboardheight,gameboardwidth,DVDlogopositiony,DVDlogopositionx).join('<br>');
         
-        //if knight is at the end of the board, change direction
-        if (knightpositiony === gameboardheight-1){
-            knightdirectiony = -1;
+        //if DVDlogo is at the end of the board, change direction
+        if (DVDlogopositiony === gameboardheight-1){
+            DVDlogodirectiony = -1;
+            document.querySelector('#gameboard').style.background = random_bg_color();
             if (Math.floor(Math.random() * 2) === 0){
-                knightpositiony--;
+                DVDlogopositiony--;
             }
         }
-        if (knightpositionx === gameboardwidth-1){
-            knightdirectionx = -1;
+        if (DVDlogopositionx === gameboardwidth-1){
+            DVDlogodirectionx = -1;
+            document.querySelector('#gameboard').style.background = random_bg_color();
             if (Math.floor(Math.random() * 2) === 0){
-                knightpositionx--;
+                DVDlogopositionx--;
             }
         }
-        if (knightpositiony === 0){
-            knightdirectiony = 1;
+        if (DVDlogopositiony === 0){
+            DVDlogodirectiony = 1;
+            document.querySelector('#gameboard').style.background = random_bg_color();
             if (Math.floor(Math.random() * 2) === 0){
-                knightpositiony++;
+                DVDlogopositiony++;
             }
         }
-        if (knightpositionx === 0){
-            knightdirectionx = 1;
+        if (DVDlogopositionx === 0){
+            DVDlogodirectionx = 1;
+            document.querySelector('#gameboard').style.background = random_bg_color();
             if (Math.floor(Math.random() * 2) === 0){
-                knightpositionx++;
+                DVDlogopositionx++;
             }
         }
-        knightpositiony=knightpositiony+knightdirectiony;
-        knightpositionx=knightpositionx+knightdirectionx;
+        DVDlogopositiony=DVDlogopositiony+DVDlogodirectiony;
+        DVDlogopositionx=DVDlogopositionx+DVDlogodirectionx;
     }, 350);
 }
 
